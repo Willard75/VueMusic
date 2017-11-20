5d5741f488f77dd93ded65c93967993d1f8a75b1
Daniel Mateu Elizalde <Dantey@MacBook-Pro-de-Daniel-3.local>
2017-11-21 00:42:34 +0100

    Fixes for the URL

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
  <template lang="pug">
  #app
-   img(src='https://willard75.github.io/VueMusic/dist/logo.png')
+   img(src='dist/logo.png')
    h1 PlatziMusic
    v-select(value='value', v-bind:input='onInput')
      v-option(value='1') option 1
